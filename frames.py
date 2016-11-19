f7ac228f3056fcada9671f4a9a92f074fe7a7811
Yash Agrawal <yagrawl2@gmail.com>
2016-11-19 17:02:28 -0600

    song manipulation

--- frames.py
+++ frames.py
@@ -1,13 +1,14 @@
 import imageio
 from upload import sendImages 
 import binascii
+from pydub import AudioSegment
 
-filename = 'IDShots.mp4'
+filename = 'Bike.mp4'
 vid = imageio.get_reader(filename,  'ffmpeg')
 length =  vid.get_length()
 fps = vid.get_meta_data()['fps']
 images = []
-for num in range(0, length, int(10*fps)):
+for num in range(0, length, int(5*fps)):
     images.append(imageio.core.util.asarray(vid.get_data(num)))
 
 images_bytes = []
@@ -38,4 +39,30 @@ for image in r['outputs']:
 	concept_counts[word] = concept_counts.get(word, 0) + 1
 	#print (concept_counts[word])
 print concept_counts
-			
+
+def argmax(concept_counts):
+	return max(concept_counts.iterkeys(), key=(lambda key: concept_counts[key]))
+
+if(argmax(concept_counts) == 'action'):
+	song = AudioSegment.from_mp3("songs/Action1.mp3")
+elif(argmax(concept_counts) == 'happy'):
+	song = AudioSegment.from_mp3("songs/Happy1.mp3")
+elif(argmax(concept_counts) == 'sad'):
+	song = AudioSegment.from_mp3("songs/Sad1.mp3")
+elif(argmax(concept_counts) == 'calm'):
+	song = AudioSegment.from_mp3("songs/Calm1.mp3")
+
+vidLen = int(length/fps)
+songLen = int(song.duration_seconds)
+
+while(songLen < vidLen):
+	vidSong += song
+	songLen = int(vidSong.duration_seconds)
+
+if(songLen >= vidLen):
+	vidSong = vidSong[:vidLen]
+
+
+
+
+

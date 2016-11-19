cd8606a5aeb2e695f9a394cce5a38649e91d2f4f
Yash Agrawal <yagrawl@dhcp-10-105-80-181.wireless.northwestern.private>
2016-11-19 10:37:26 -0600

    Extract frames as raw bytes

--- frames.py
+++ frames.py
@@ -1,11 +1,19 @@
 import matplotlib.pyplot as plt
 import imageio
+from upload import sendImages 
+
 filename = 'IDShots.mp4'
 vid = imageio.get_reader(filename,  'ffmpeg')
 length =  vid.get_length()
 fps = vid.get_meta_data()['fps']
+images = []
 for num in range(0, length, int(10*fps)):
-    image = vid.get_data(num)
-    fig = plt.figure()
-    plt.imshow(image)
-plt.show()
+    images.append(imageio.core.util.asarray(vid.get_data(num)))
+
+images_bytes = []
+for i in range(len(images)):
+    file_name = "temp/" + str(i) 
+    images_bytes.append(imageio.imwrite(imageio.RETURN_BYTES, images[i], "jpg"))
+
+
+sendImages(images_bytes)

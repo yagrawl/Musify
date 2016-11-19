4adeb341764a522264df0cd7011e05595e12de28
Yash Agrawal <yagrawl@dhcp-10-105-80-181.wireless.northwestern.private>
2016-11-19 10:07:27 -0600

    added fps

--- frames.py
+++ frames.py
@@ -3,10 +3,9 @@ import imageio
 filename = 'IDShots.mp4'
 vid = imageio.get_reader(filename,  'ffmpeg')
 length =  vid.get_length()
-print vid.get_meta_data().fps
-# for num in range(0, length, 5*30):
-#     image = vid.get_data(num)
-#     fig = plt.figure()
-#     #fig.suptitle('image #{}'.format(num), fontsize=20)
-#     plt.imshow(image)
-# plt.show()
+fps = vid.get_meta_data()['fps']
+for num in range(0, length, int(10*fps)):
+    image = vid.get_data(num)
+    fig = plt.figure()
+    plt.imshow(image)
+plt.show()
